8fc1a514abc2b7ce30f46fbb6934d4a7230410a2
Tony DUONG <astowny+oracle-flex@gmail.com>
2024-05-25 23:14:25 +0000

    restructure folder for production

--- myapp/app.py
+++ myapp/app.py
@@ -19,10 +19,11 @@ def predict_deepfake_from_file(images):
     else:
         return 'Prediction error', None, 400  # Return None if no predictions were made
   
-app = Flask(__name__)
- 
+app = Flask(__name__,template_folder='templates')
+app.logger('some debug after flask app') 
 @app.route('/')
 def index():
+    app.logger(app.template_folder)
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
